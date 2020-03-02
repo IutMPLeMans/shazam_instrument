03b8fb197a1d3f7c7bc5cc53dd2ab4b96ad3b21b
i181267 <61693926+i181267@users.noreply.github.com>
2020-03-02 17:23:19 +0100

    test

---
+++ shazam_instrument/module1.py
@@ -0,0 +1,31 @@
+import wx
+
+class MaFenetre(wx.Frame):    
+    def __init__(self):
+        super().__init__(parent=None, title='Ma fenêtre avec bouton')
+        
+        nb_boutons = 16
+        self.ma_grille = wx.GridSizer(rows=4, cols=4, vgap=10, hgap=10)
+        font = wx.Font(20, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
+            wx.FONTWEIGHT_BOLD)
+        for i in range(nb_boutons):
+            bouton = wx.Button(self,  label='X')
+            bouton.SetFont(font)
+            bouton.Bind(wx.EVT_BUTTON, self.OnBouton)            
+            self.ma_grille.Add(bouton, i,wx.EXPAND)          
+           
+        self.SetSizerAndFit(self.ma_grille)        
+        self.Show()
+
+    def OnBouton(self, event):
+        bouton = event.GetEventObject()
+        s = bouton.GetLabel()
+        if s == "X":
+            bouton.SetLabel("O")
+        elif s == "O":
+            bouton.SetLabel("X")
+       
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MaFenetre()
+    app.MainLoop()
